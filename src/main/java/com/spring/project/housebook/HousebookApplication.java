0b2cac65258b1e7d960914e1813d3a70c8ea8d6e
masakiShito <happy.engineer.life@gmail.com>
2023-05-16 06:10:35 +0900

    プロジェクト作成
    プロジェクトの新規作成
    動作確認実施済み

---
+++ src/main/java/com/spring/project/housebook/HousebookApplication.java
@@ -0,0 +1,13 @@
+package com.spring.project.housebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/project/housebook/HousebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.project.housebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
